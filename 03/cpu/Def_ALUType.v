e782cce992616c88515e050d5660c97bc3a4922f
Akshit Talwar <akshittalwar@protonmail.com>
2022-06-11 19:21:24 +0530

    cpu alu

---
+++ 03/cpu/Def_ALUType.v
@@ -0,0 +1,18 @@
+// Constants that are using in the ALU
+
+`define ALUType_And 4'b0000
+`define ALUType_Eor 4'b0001
+`define ALUType_Sub 4'b0010
+`define ALUType_Rsb 4'b0011
+`define ALUType_Add 4'b0100
+`define ALUType_Adc 4'b0101
+`define ALUType_Sbc 4'b0110
+`define ALUType_Rsc 4'b0111
+`define ALUType_Tst 4'b1000
+`define ALUType_Teq 4'b1001
+`define ALUType_Cmp 4'b1010
+`define ALUType_Cmn 4'b1011
+`define ALUType_Orr 4'b1100
+`define ALUType_Mov 4'b1101
+`define ALUType_Bic 4'b1110
+`define ALUType_Mvn 4'b1111
---
+++ 03/cpu/Def_StructureParameter.v
@@ -0,0 +1,6 @@
+// Constants that define the architectural decisions and structure of
+// instructions
+
+`define WordWidth 32
+`define ByteWidth 8
+`define WordZero 32'h0000_0000
